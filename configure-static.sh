8ef17e250cc4626fa3ad0c27851f5de56be5e123
Sam Heybey <sam@heybey.org>
2019-03-12 18:43:47 -0400

    configure-static.sh: use set -e and set -x

--- configure-static.sh
+++ configure-static.sh
@@ -1,32 +1,27 @@
 #!/bin/bash
-# Copyright (c) 2017-2018 Thought Networks LLC
+# Copyright (c) 2017-2019 Thought Networks LLC
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-echo_run() {
-	echo - $@
-	$@
-	return $?
-}
+set -e
+set -x
 
 BASEDIR=$(pwd)
 
 if [ -f Makefile ]; then
-	echo_run make distclean
+	make distclean
 fi
 
 if [ "x${HOST}y" = "xy" ]; then
-	echo_run export HOST="$(./depends/config.guess)"
+	export HOST="$(./depends/config.guess)"
 fi
 
-echo_run cd depends
-echo_run make
-echo_run cd ..
+cd depends
+make
+cd ..
 
 if [ '!' -f configure ]; then
-	echo_run ./autogen.sh
+	./autogen.sh
 fi
 
-echo - env "CONFIG_SITE=$BASEDIR/depends/$HOST/share/config.site" "LDFLAGS=-static-libgcc -static-libstdc++" ./configure --prefix=/
-env "CONFIG_SITE=$BASEDIR/depends/$HOST/share/config.site" "LDFLAGS=-static-libgcc -static-libstdc++" ./configure --prefix=/ $@
-
+./configure "--prefix=$BASEDIR/depends/$HOST" $@
