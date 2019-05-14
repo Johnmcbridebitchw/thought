99fa314a7b53b40494c78d95ffa7099d02752b46
Phil Grim <pagrim@mistiqtech.com>
2019-05-14 16:19:32 -0400

    Copyright notice updates.

--- src/bench/crypto_hash.cpp
+++ src/bench/crypto_hash.cpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2016 The Thought Core developers
-// Copyright (c) 2018 The Thought Core developers
+// Copyright (c) 2018 The Dash Core developers
+// Copyright (c) 2019 Thought Networks, LLC
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
