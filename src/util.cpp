092b1047464e1e5a32bc0359b4e4ebbb1d0d2de4
Phil Grim <phil@insufficient-light.com>
2022-03-28 19:21:38 -0400

    Fix up copyright output.

--- src/util.cpp
+++ src/util.cpp
@@ -980,8 +980,9 @@ std::string CopyrightHolders(const std::string& strPrefix, unsigned int nStartYe
     std::string strCopyrightHolders = strPrefix + strprintf(" %u-%u ", nStartYear, nEndYear) + strprintf(_(COPYRIGHT_HOLDERS), _(COPYRIGHT_HOLDERS_SUBSTITUTION));
 
     // Check for untranslated substitution to make sure Thought Core copyright is not removed by accident
-    if (strprintf(COPYRIGHT_HOLDERS, COPYRIGHT_HOLDERS_SUBSTITUTION).find("Thought Network Ltd") == std::string::npos) {
-        strCopyrightHolders += "\n" + strPrefix + strprintf(" %u-%u ", 2009, nEndYear) + "The Dash Core developers";
+    if (strprintf(COPYRIGHT_HOLDERS, COPYRIGHT_HOLDERS_SUBSTITUTION).find("Core") == std::string::npos) {
+        strCopyrightHolders += "\n" + strPrefix + " 2014-2017 The Dash Core developers"
+		            + "\n" + strPrefix + " 2009-2015 The Bitcoin Core developers";
     }
     return strCopyrightHolders;
 }
